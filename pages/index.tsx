b7effeecf007a709b4a5a1ee277598d9f7b2bb99
TarrenHassman <tarren.m.hassman@gmail.com>
2023-08-01 14:57:39 -0700

    Began removal of non-commercial assets, and added parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,72 @@ import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import { NextPage } from "next";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import Navbar from "../components/navbar";
 
 const Home: NextPage = () => {
   return (
-    <Parallax pages={4}>
+    <Parallax pages={10}>
       <ParallaxLayer
-      factor={3}
+      sticky={{
+        start:0,
+        end: 10
+      }}
+      >
+      <Image className={styles.frontZ}
       style={{
-        backgroundImage: "url('/images/ConceptArtq.jpg')",
+        float:"right",
+        transform:"translate(-3vw, 85vh)"
+      }}
+      src="/images/Key.png"
+      alt="Placeholder preview of starter"
+      width={100}
+      height={100}
+      />
+      <Navbar></Navbar>
+      </ParallaxLayer>
+      <ParallaxLayer
+      factor={10}
+      style={{
+        backgroundImage: "url('/images/background.jpg')",
         backgroundSize: '100% 100%'
       }}
       >
       </ParallaxLayer>
       <ParallaxLayer
       speed={.5}
+      factor={3}
       offset={1}
+            style={{
+              backgroundImage: "url('/images/Aether1.png')",
+              backgroundSize: '100% 100%'
+            }}
+      >
+      </ParallaxLayer>
+      <ParallaxLayer
+      speed={1}
+      factor={.3}
+      offset={2}
+            style={{
+              backgroundImage: "url('/images/Aether1.png')",
+              backgroundSize: '50% 100%'
+            }}
+      >
+        <p>
+          Information about my new game
+        </p>
+        <p
+        style={{
+          float:"right",
+          transform: "translate(0px, 50vh)"
+        }}
+        >
+          Information about my new game
+        </p>
+      </ParallaxLayer>
+      <ParallaxLayer
+      speed={.2}
+      factor={5}
+      offset={7}
             style={{
               backgroundImage: "url('/images/Aether1.png')",
               backgroundSize: '100% 100%'
@@ -28,18 +79,6 @@ const Home: NextPage = () => {
     </Parallax>
   );
 };
-      {/* <Image className={styles.frontZ}
-      src="/images/Key.png"
-      alt="Placeholder preview of starter"
-      width={100}
-      height={100}
-      /> */}
 
-      {/* <Image className={styles.backZ}
-        src="/images/ConceptArtq.jpg"
-        alt="Placeholder preview of starter"
-        fill
-        quality={100}
-      /> */}
 
 export default Home;
