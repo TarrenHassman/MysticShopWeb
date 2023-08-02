a257ecb86ec429dbeb2b4ec529091912274d033c
TarrenHassman <tarren.m.hassman@gmail.com>
2023-08-02 08:26:17 -0700

    asdfadf

--- components/claimCard.tsx
+++ components/claimCard.tsx
@@ -41,10 +41,19 @@ export default function ClaimCard() {
             </div>
             {!isActiveClaimPhaseLoading && (
               <div>
-                <p>Claim Phase: {activeClaimPhase?.metadata?.name}</p>
+                {/* <p>Claim Phase: {activeClaimPhase?.metadata?.name}</p> */}
                 <p>Price: {ethers.utils.formatUnits(activeClaimPhase?.price!)}</p>
               </div>
             )}
+            {
+              !isTotalClaimedLoading && (
+                <div>
+                  <p>
+                    Claimed: {totalClaimed?.toNumber()}
+                  </p>
+                </div>
+              )
+            }
             
           </div>
         )}
