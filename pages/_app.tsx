2b576f98cc2df24f4fabc50b7a092f75ee2db3fc
TarrenHassman <tarren.m.hassman@gmail.com>
2023-08-01 23:32:28 -0700

    asdfasdjfiklhasdf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       supportedWallets={[metamaskWallet(), coinbaseWallet(), walletConnect(),safeWallet()]}
-      clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
+      clientId={"d701b43f9b5960e8126324e5e030dbf6"}
       activeChain={activeChain}
     >
       <Component {...pageProps} />
