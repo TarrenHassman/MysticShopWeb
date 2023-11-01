7836d1239fe39aee0e65ac2438de2d16e0930d65
TarrenHassman <tarren.m.hassman@gmail.com>
2023-10-31 20:51:54 -0700

    Profile swap

--- pages/GlassProfile.js
+++ pages/GlassProfile.js
@@ -8,44 +8,47 @@ function GlassProfile() {
   }
   return <div className= {styles.container1 + " " + styles.close + " " + neumorphic.undented} id="glassProfile">
     <div className={styles.wrapper1}>
-      <a href="a">
-        <img alt="Avatar" src="favicon.ico" className={styles.avatar} />
-      </a>
+      <div>
+      <img alt="Avatar" src="favicon.ico" className={styles.avatar} />
+      </div>
+       
       <button className={styles.collapseArrow} onClick={expand}>
         <img src="expand.svg" />
       </button>
       <div className={styles.text1}>
         <div className={styles.icons1}>
           <li >
-            <p>      <ConnectWallet className={styles.connectWallet} modalSize={"compact"}></ConnectWallet></p>
-            <button>
+          <button>
               <img src="balance.svg" ></img>
-
             </button>
+            <p>      <ConnectWallet className={styles.connectWallet} modalSize={"compact"}></ConnectWallet></p>
           </li>
           <li >
+          <button >
+            <img src="pouch.jpg" ></img>
+            </button>
             <p>Inventory</p>
-            <button >
-            <img src="group.svg" ></img>
-            </button>
           </li>
           <li >
+          <button >
+            <img src="swap.svg" ></img>
+            </button>
             <p>Trade Requests</p>
-            <button >
-            <img src="cart.svg" ></img>
-            </button>
+        
           </li>
           <li >
-            <p>World</p>
-            <button >
+          <button >
             <img src="world.svg" ></img>
             </button>
+            <p>World</p>
+     
           </li>
           <li >
-            <p>Shopping Cart</p>
-            <button >
+          <button >
             <img src="cart.svg" ></img>
             </button>
+            <p>Shopping Cart</p>
+     
           </li>
         </div>
       </div>
